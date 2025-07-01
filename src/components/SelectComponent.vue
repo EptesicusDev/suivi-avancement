e64cc7d2e44ba554db75bab4b9688dd2bed67417
Martin BALLU <eptesicus.dev@gmail.com>
2025-07-01 09:04:44 +0000

    fix: zone selector hide when printing

--- src/components/SelectComponent.vue
+++ src/components/SelectComponent.vue
@@ -43,7 +43,7 @@ onClickOutside(target, () => {
         <div class="flex h-10">
             <PrintButtonComponent @print="print"/>
             <button v-show="props.options.length > 1" type="button" @click="toggleDropdown()"
-                class="ml-1 cursor-pointer inline-flex w-max justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm text-zinc-900 shadow-sm border border-zinc-300 ring-inset hover:bg-gray-50"
+                class="zone-selector ml-1 cursor-pointer inline-flex w-max justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm text-zinc-900 shadow-sm border border-zinc-300 ring-inset hover:bg-gray-50"
                 id="menu-button" aria-expanded="true" aria-haspopup="true">
                 {{ formatZone(store.currentWorkshop.zone) }}
                 <svg class="-mr-1 size-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
